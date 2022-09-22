e3a1aa24debb31c6a6a9b936ec3cb8fa42e21ad4
Shubham-code-lab <shubhamshinde281999@gmail.com>
2022-09-22 07:08:23 +0530

    edit feature for admin

--- 9/controllers/admin.js
+++ 9/controllers/admin.js
@@ -1,9 +1,20 @@
 const Product = require('../models/product');
 
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
+
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    edit: false
   });
 };
 
@@ -17,12 +28,23 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+exports.getEditProduct = (req, res, next)=>{
+  const prodId = req.params.productId;    //"12345"
+  const edit = !!req.query.edit;           //!!"true"  === true   (string is not empty so it is true)  !"true" === false
+  console.log(req.params);
+  console.log(prodId,edit);
+  if(!edit)return;
+  Product.findById(prodId,(product)=>{
+    if(!product)return;
+    res.render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      edit,
+      product
     });
   });
+};
+
+exports.postEditProduct = (req, res, next)=>{
+  console.log("hii");
 };
