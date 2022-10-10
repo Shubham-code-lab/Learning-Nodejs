11f7a705fedecae37f54e9c7f28ffd9205aa46ca
Shubham Shinde <shubhamshinde281999@gmail.com>
2022-10-10 22:33:08 +0530

    monogdb create and update query

--- 12/controllers/admin.js
+++ 12/controllers/admin.js
@@ -13,14 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
-    .then(result => {
+  const product = new Product(title, imageUrl, price, description);
+  product.save()
+  .then(result => {
       // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
@@ -36,14 +31,13 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findByPk(prodId)
-    .then(products => {
-      const product = products[0];
+  console.log("get edit id",prodId);
+  Product.findById(prodId)
+    .then(product => {        //{}
       if (!product) {
         return res.redirect('/');
       }
+      console.log("get Edit",product);
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
@@ -60,14 +54,23 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+ 
+  // Product.findById(prodId)
+  // .then(product=>{
+  //   const id = product._id;
+  console.log("post Edit", prodId);
+     const newProduct = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, prodId);
+     newProduct.save()
+  // })
+
+  // Product.findByPk(prodId)
+  //   .then(product => {
+  //     product.title = updatedTitle;
+  //     product.price = updatedPrice;
+  //     product.description = updatedDesc;
+  //     product.imageUrl = updatedImageUrl;
+  //     return product.save();
+  //   })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -76,8 +79,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+    Product.fetchAll()  
     .then(products => {
       res.render('admin/products', {
         prods: products,
--- 12/models/product.js
+++ 12/models/product.js
@@ -1,19 +1,55 @@
+const mongodb = require('mongodb');
 const getDB = require('../util/database').getDB;
 
 class Product{
-  constructor(title, price ,description, imgeUrl){
+  constructor(title, imageUrl, price ,description, id){
     this.title =title;
     this.price = price;
     this.description = description;
-    this.imgeUrl = imgeUrl;
+    this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save(){
     const db = getDB();
-    db.collection('products').insertOne(this)  //this is just object  //insertOne({})   //insertMany([{},{}])
-    .then(result=>console.log(result))
+    let dbOP;
+    if(this._id){  //update
+      dbOP = db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this})  //OR {$set:{title:this.title,......}}
+    }
+    else{
+      // add to existing collecction or create new collection
+       dbOP = db.collection('products').insertOne(this)  //this is just object  //insertOne({})   //insertMany([{},{}])
+    }
+    return dbOP.then(result=>console.log(result))
     .catch(err=>console.log(err))
   }
+
+  static fetchAll(){
+    const db = getDB();
+    return db.collection('products')  //which collection to connect to
+    .find()       //find({title:'A book'})  to filter result  //find() return cursor and object provided by mongodb so as to not return all the document at once (to avoid million of data being fetched)
+    .toArray()    //get all document and turn them into array
+    .then(products=>{
+      console.log("fetchAll",products);
+      return products
+    })
+    .catch(err=>console.log(err));
+  }
+
+  static findById(prodId){
+    const db = getDB();
+    return db.collection('products')
+    //mongodb uses bSOn format not only because it is fast but also it store special type of data(dataType) ObjectId so we have to type cast it
+    //mongodb uses ObjectId to handle/manage ID hence _id is type of ObjectId
+    .find({_id: new mongodb.ObjectId(prodId)})  //return cursor even when only one document we get
+    .next()  //we get next or last document instead of cursor
+    .then(product=>{                //{}
+      console.log("findById",product)
+      return product
+    })
+    .catch(err=>console.log(err))
+  }
+
 }
 
 module.exports = Product;
